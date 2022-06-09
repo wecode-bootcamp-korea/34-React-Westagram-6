df1ea7c0e60b1991b99595e5e4f917ca5f480cd8
beomq <beomdolq@gmail.com>
2022-06-09 17:08:44 +0900

    마지막 수정 완료했습니다

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './Router';
-import Login from './pages/beomseok/Login/Login';
-import Main from './pages/beomseok/Main/Main';
 import './styles/common.scss';
 import './styles/reset.scss';
 import './styles/variables.scss';
