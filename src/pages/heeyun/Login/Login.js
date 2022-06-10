01b92d3735987c0f1c1d0397b80c30a56b5f0ea4
hongheeyun <namu2267@gmail.com>
2022-06-10 17:48:02 +0900

    위스타그램 로그인,메인페이지 리액트,라우터했습니다

--- src/pages/heeyun/Login/Login.js
+++ src/pages/heeyun/Login/Login.js
@@ -1,8 +1,39 @@
 import React from 'react';
-import './login.scss';
+import './login.css';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-  return <div>heeyun</div>;
+  const navigate = useNavigate();
+
+  const goToMain = () => {
+    navigate('/main');
+  };
+
+  return (
+    <div class="loginWrapper">
+      <div class="container">
+        <div class="top">
+          <h2>Westagram</h2>
+          <div class="inputbox">
+            <input
+              class="idInput"
+              type="text"
+              placeholder="전화번호, 사용자 이름 또는 이메일"
+            />
+            <input class="idInput" type="text" placeholder="비밀번호" />
+          </div>
+          <div class="btn">
+            <button class="loginbutton" onClick={goToMain}>
+              로그인
+            </button>
+          </div>
+        </div>
+        <div class="bottom">
+          <span>비밀번호를 잊으셨나요?</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
