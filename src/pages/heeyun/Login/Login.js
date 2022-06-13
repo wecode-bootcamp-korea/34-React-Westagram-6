28bdb0cf53e200be4ebf2057db4de1780627e068
hongheeyun <namu2267@gmail.com>
2022-06-13 22:13:48 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/heeyun/Login/Login.js
+++ src/pages/heeyun/Login/Login.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
-import './login.css';
+import './login.scss';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const [id, setId] = useState('');
+  const [password, setPw] = useState('');
+  // const [disabled, setDisabled] = useState(true);
+
   const navigate = useNavigate();
+
+  const valid = id.includes('@') && password.length > 4;
 
   const goToMain = () => {
     navigate('/Main-heeyun');
   };
-
-  const [id, setId] = useState('');
-  const [password, setPw] = useState('');
 
   // const handleIdInput = e => {
   //   setId(e.target.value);
@@ -25,25 +28,33 @@ const Login = () => {
       <div className="container">
         <div className="top">
           <h2>Westagram</h2>
-          <div className="inputbox">
-            <input
-              className="idInput"
-              type="text"
-              placeholder="전화번호, 사용자 이름 또는 이메일"
-              onChange={e => setId(e.target.value)}
-            />
-            <input
-              className="idInput"
-              type="text"
-              placeholder="비밀번호"
-              onChange={e => setPw(e.target.value)}
-            />
-          </div>
-          <div className="btn">
-            <button className="loginbutton" onClick={goToMain}>
+          <form
+            onSubmit={event => {
+              event.preventDefault();
+              goToMain();
+            }}
+          >
+            <div className="inputbox">
+              <input
+                className="idInput"
+                type="text"
+                placeholder="전화번호, 사용자 이름 또는 이메일"
+                onChange={e => setId(e.target.value)}
+              />
+              <input
+                className="idInput"
+                type="text"
+                placeholder="비밀번호"
+                onChange={e => setPw(e.target.value)}
+              />
+            </div>
+            <button
+              className={valid ? 'loginAbled' : 'loginDisabled'}
+              // disabled={disabled}
+            >
               로그인
             </button>
-          </div>
+          </form>
         </div>
         <div className="bottom">
           <span>비밀번호를 잊으셨나요?</span>
@@ -54,3 +65,33 @@ const Login = () => {
 };
 
 export default Login;
+
+// <form className="inputbox">
+//             <input
+//               className="idInput"
+//               type="text"
+//               placeholder="전화번호, 사용자 이름 또는 이메일"
+//               onChange={e => setId(e.target.value)}
+//             />
+//             <input
+//               className="idInput"
+//               type="text"
+//               placeholder="비밀번호"
+//               onChange={e => setPw(e.target.value)}
+//             />
+//
+//
+//             <button
+//               className="loginbtn"
+//               disabled={disabled}
+//               onClick={() => {
+//                 if (setId.includes('@') >= 0 && setPw.length > 4) {
+//                   setDisabled(true);
+//                 } else {
+//                   setDisabled('false');
+//                 }
+//               }}
+//             >
+//               로그인
+//             </button>
+//          </form>
